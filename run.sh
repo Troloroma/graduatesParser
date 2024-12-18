ea1af777f777da2d28f93c41d0da2fb96cac3f6b
Kakashius <nakadashi@mail.ru>
2024-12-19 02:20:10 +0300

    final final fix

--- run.sh
+++ run.sh
@@ -71,6 +71,7 @@ while true; do
   fi
 
   if [ "$PARSER_STATUS" == "exited" ] && [ "$ANALYSIS_STATUS" == "exited" ] && [ "$DB_STATUS" == "exited" ]; then
+    docker-compose down
     echo "Все контейнеры завершили свою работу."
     break
   fi
